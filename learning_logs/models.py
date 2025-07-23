dd6a8f89ae1d2615559f6d8cb927534290c25736
Mbelenga <mbelengaterence@gmail.com>
2025-07-24 01:59:19 +0300

    string

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,3 +6,7 @@ class Topic(models.Model):
     """ A topic the user is learning about """
     text = models.CharFields(max_length=200)
     date_added = models.DateTimeField(auto_now_add = True)
+
+def __str__(self):
+    """ Returns a string representation of the model """
+    return self.text
