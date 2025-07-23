c534ce135351870150c57c1c636a42c5bafb6c40
Mbelenga <mbelengaterence@gmail.com>
2025-07-24 02:09:49 +0300

    Date Time Field

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Topic(models.Model):
     """ A topic the user is learning about """
     text = models.CharFields(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True) # tells Django to automatically set this attribute to the current date and time
+    date_added = models.DateTimeField(auto_now_add=True)
+    """ tells Django to automatically set this attribute to the current date and time """
 whenever the user creates a new topic
 
 def __str__(self):
