8ea7c73a6b1da905b25d95e1a39999ba2881e8fd
Mbelenga <mbelengaterence@gmail.com>
2025-07-26 23:42:15 +0300

    Progress

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, Entry
+"""
+The dot in front of models tells Django
+to look for models.py in the same directory as admin.py. 
+"""
+
+admin.site.register(Topic)
+admin.site.register(Entry)
+"""
+This code admin.site.register() tells Django to manage our model
+through the admin site.
+"""
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,11 +4,24 @@ from django.db import models
 
 class Topic(models.Model):
     """ A topic the user is learning about """
-    text = models.CharFields(max_length=200)
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    """ tells Django to automatically set this attribute to the current date and time """
-whenever the user creates a new topic
+    """
+    tells Django to automatically set this attribute to the current date and time
+    whenever the user creates a new topic
+    """
+
+class Entry(models. Model):
+    """ Something specific learned about a topic. """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    verbose_name_plural = 'entries'
+
+
 
 def __str__(self):
     """ Returns a string representation of the model """
-    return self.text
+    return f"{self.text[:50]}..."
