5818b944a9c7ec65062893b299e6a524a5c157be
Mbelenga <mbelengaterence@gmail.com>
2025-08-05 23:36:50 +0300

    Updated

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -16,4 +16,4 @@ def topic(request, topic_id):
     topics = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/', context)
+    return render(request, 'learning_logs/topic.html', context)
