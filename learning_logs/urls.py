98014cfec580c99dcb8d06b24e1514e7bd1457f7
Mbelenga <mbelengaterence@gmail.com>
2025-08-06 12:49:15 +0300

    Page for adding a new topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # Details page for a single topic
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
 
 ]
